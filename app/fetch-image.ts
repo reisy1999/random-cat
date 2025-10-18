91ee0293eaef92ca851875bb96d3f031fde46bbb
reisy1999 <abereneibu@gmail.com>
2025-10-18 23:47:35 +0900

    CatImageProps型にあったurl変数にそってreturnする関数。returnするのはsrc属性にurlの中身をセットすること

--- app/fetch-image.ts
+++ app/fetch-image.ts
@@ -1,6 +1,10 @@
-// APIから画像を取得する関数の実装
+// 画像情報の型定義
+type Image= {
+    url: string;
+};
 
-export async function fetchImage() {
+// APIから画像を取得する関数の実装
+export async function fetchImage(): Promise<Image> {
     const res = await  fetch("https://api.thecatapi.com/v1/images/search");
     const images = await res.json();
     console.log("fetchImage: 画像譲歩を取得しました", images);
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { connection } from "next/server";
 import { fetchImage } from "./fetch-image";
 
 export default async function Home() {
-  await connection();
+  await connection(); //　これでリロードのたびにapiを呼び出せる
   const image = await fetchImage();
   console.log("画像情報を取得しました", image);
   return <div> 猫画像予定地</div>;
