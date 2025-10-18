d3c1bad54061453eb459056b066bb72257353a43
reisy1999 <abereneibu@gmail.com>
2025-10-18 21:34:07 +0900

    リロードのたびにapiをたたきたい

---
+++ app/fetch-image.ts
@@ -0,0 +1,8 @@
+// APIから画像を取得する関数の実装
+
+export async function fetchImage() {
+    const res = await  fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log("fetchImage: 画像譲歩を取得しました", images);
+    return images[0];
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import { connection } from "next/server";
+import { fetchImage } from "./fetch-image";
+
+export default async function Home() {
+  await connection();
+  const image = await fetchImage();
+  console.log("画像情報を取得しました", image);
   return <div> 猫画像予定地</div>;
 }
