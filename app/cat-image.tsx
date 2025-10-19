f183f5cc9ba5b4e1383725af0e2f0a330003c06f
reisy1999 <abereneibu@gmail.com>
2025-10-19 11:44:27 +0900

    useStateによる状態管理とrefreshによる初期化

---
+++ app/cat-image.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {useState } from "react";
+import { fetchImage } from "./fetch-image";
+
+//　コンポーネントの引数を定義する
+type CatImageProps = {
+    url: string;
+};
+
+// 画像を表示するコンポーネント
+export function CatImage({ url }: CatImageProps) {
+    const [ imageUrl, setImageUrl] = useState(url);
+
+    const refreshImage = async () => {
+        setImageUrl("");
+        const image = await fetchImage();
+        setImageUrl(image.url);
+    };
+
+    return (
+        <div>
+            {}
+            <button onClick={refreshImage}>他の猫もみる</button>
+            {}
+            {imageUrl && <img src={imageUrl} />} 
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { connection } from "next/server";
+import { CatImage } from "./cat-image";
 import { fetchImage } from "./fetch-image";
 
 export default async function Home() {
   await connection(); //　これでリロードのたびにapiを呼び出せる
   const image = await fetchImage();
-  console.log("画像情報を取得しました", image);
-  return <div> 猫画像予定地</div>;
+  // 画像のURLを渡す
+  return <CatImage url={image.url} />;
 }
